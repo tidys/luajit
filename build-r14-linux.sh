cd14778a69016a873b4a05dfffae5039c64b67d1
tidy <xu_yanfeng@126.com>
2023-02-15 00:10:38 +0800

    3453535

--- build-r14-linux.sh
+++ build-r14-linux.sh
@@ -1,8 +1,10 @@
 # Android/ARM, armeabi-v7a (ARMv7 VFP), Android 4.1+ (JB)
 make clean
 $(info "build r14 linux ...")
+# NDK=F:/android-ndk-r14b-linux
+NDK=$(pwd)/android-ndk-r14b
+info(info $NDK)
 
-NDK=F:/android-ndk-r14b-linux
 NDKBIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
 NDKABI=21
 NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
