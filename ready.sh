3445af2c7b9e23279a192528e96d601bca155875
cc <xu_yanfeng@126.com>
2023-02-15 11:40:44 +0800

    fff

---
+++ ready.sh
@@ -0,0 +1,7 @@
+wget https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip
+
+unzip android-ndk-r14b-linux-x86_64.zip -d ./
+
+chmod +x ./build-r14-linux.sh
+
+./build-r14-linux.sh
